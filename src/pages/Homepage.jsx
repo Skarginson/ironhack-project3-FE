0f02de0d5b0d163e0851be754b3de8a1ce849d5b
Skarginson <eldaraes@gmail.com>
2024-05-30 09:47:13 +0200

    pages + routes + cleanup

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Sidebar from "../components/Sidebar";
+
+function Homepage() {
+  return (
+    <>
+      <Header />
+      <Sidebar />
+      <Footer />
+    </>
+  );
+}
+
+export default Homepage;
