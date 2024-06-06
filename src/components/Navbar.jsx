5e667891eda4151b59e9a0238c26cca4b6aad3d1
Skarginson <eldaraes@gmail.com>
2024-06-06 15:46:17 +0200

    logout button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,22 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 import styles from "../styles/Navbar.module.css";
+import { AuthContext } from "../contexts/AuthContext";
 
 function Navbar() {
+  const { updateToken } = useContext(AuthContext);
+
+  const handleLogout = () => {
+    updateToken(null);
+  };
+
   return (
     <nav className={styles.navbar}>
       <Link to="/signup">Signup</Link>
       <Link to="/about">About</Link>
-      <Link to="/logout">LogOut</Link>
+      <button onClick={handleLogout} className={styles.linkButton}>
+        LogOut
+      </button>
     </nav>
   );
 }
